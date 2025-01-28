69af0ad0094b7992e2b7c7ae4ba2a9136eaed9f9
abhishek <abhishek.s@wizcommerce.com>
2025-01-28 12:33:24 +0530

    feat: added 2 page template

--- page_2/data.py
+++ page_2/data.py
@@ -1,85 +1,201 @@
 data = {
-    "logo_path": "",
-    "sheet_title": "Dallas Tradeshow Collection",
-    "date": "06/25/2024",
-    "products": [
-        {
-            "name": "Twinning Sable (Large, Beige) Marble Candle Holder",
-            "sku": "WC_DC57-10003",
-            "category": "Home Decor",
-            "description": "The candle holder features a sleek and contemporary design...",
-            "features": ["Marble body", "Beige", "Cream finish"],
-            "available_units": 400,
-            "price": 32,
-            "moq": 15,
-            "image_url": "",
-            "long_text": "The candle holder features a sleek and contemporary design...",
-            "style": "Vintage, Modern, Traditional, Rustic, Bohemian",
-            "finish": "Matte",
-            "color": "Beige",
-        },
-        {
-            "name": "Twinning Sable (Large, Beige) Marble Candle Holder",
-            "sku": "WC_DC57-10003",
-            "category": "Home Decor",
-            "description": "The candle holder features a sleek and contemporary design...",
-            "features": ["Marble body", "Beige", "Cream finish"],
-            "available_units": 400,
-            "price": 32,
-            "moq": 15,
-            "image_url": "",
-            "long_text": "The candle holder features a sleek and contemporary design...",
-            "style": "Vintage, Modern, Traditional, Rustic, Bohemian",
-            "finish": "Matte",
-            "color": "Beige",
-        },
-        {
-            "name": "Twinning Sable (Large, Beige) Marble Candle Holder",
-            "sku": "WC_DC57-10003",
-            "category": "Home Decor",
-            "description": "The candle holder features a sleek and contemporary design...",
-            "features": ["Marble body", "Beige", "Cream finish"],
-            "available_units": 400,
-            "price": 32,
-            "moq": 15,
-            "image_url": "",
-            "long_text": "The candle holder features a sleek and contemporary design...",
-            "style": "Vintage, Modern, Traditional, Rustic, Bohemian",
-            "finish": "Matte",
-            "color": "Beige",
-        },
-        {
-            "name": "Twinning Sable (Large, Beige) Marble Candle Holder",
-            "sku": "WC_DC57-10003",
-            "category": "Home Decor",
-            "description": "The candle holder features a sleek and contemporary design...",
-            "features": ["Marble body", "Beige", "Cream finish"],
-            "available_units": 400,
-            "price": 32,
-            "moq": 15,
-            "image_url": "",
-            "long_text": "The candle holder features a sleek and contemporary design...",
-            "style": "Vintage, Modern, Traditional, Rustic, Bohemian",
-            "finish": "Matte",
-            "color": "Beige",
-        },
-        {
-            "name": "Twinning Sable (Large, Beige) Marble Candle Holder",
-            "sku": "WC_DC57-10003",
-            "category": "Home Decor",
-            "description": "The candle holder features a sleek and contemporary design...",
-            "features": ["Marble body", "Beige", "Cream finish"],
-            "available_units": 400,
-            "price": 32,
-            "moq": 15,
-            "image_url": "",
-            "long_text": "The candle holder features a sleek and contemporary design...",
-            "style": "Vintage, Modern, Traditional, Rustic, Bohemian",
-            "finish": "Matte",
-            "color": "Beige",
-        },
-    ],
-    "footer_link": "www.ashleyfurniture.com",
-    "footer_link_text": "www.ashleyfurniture.com",
-    "page_number": 1
-}
+        "products":
+        [
+            {
+                "product_name": "Neha Iridescent Checkered Area Rug",
+                "sku_id": "200ACOS12A-P-A230",
+                "product_details": "",
+                "product_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Grey"
+                    },
+                    {
+                        "name": "Material",
+                        "value": "100% Polyester"
+                    }
+                ],
+                "hinge_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Grey"
+                    },
+                    {
+                        "name": "Size",
+                        "value": "4x6 Inch"
+                    }
+                ],
+                "hinges_values": "Grey, 4x6 Inch",
+                "product_hinges":
+                [
+                    {
+                        "name": "Color",
+                        "variants": "Grey, Light Grey"
+                    },
+                    {
+                        "name": "Size",
+                        "variants": "4x6 Inch, 5x8 Inch, 8x10 Inch"
+                    }
+                ],
+                "is_custom_product": False,
+                "applied_modifiers":
+                [],
+                "group_name": "",
+                "group_number": 0,
+                "priority": 1,
+                "product_image_1": "https://fileserver-g-p.sourcerer.tech/files/daf1c2f9-33e5-452c-83de-c5a88395580b?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "product_image_2": "https://fileserver-g-p.sourcerer.tech/files/608b866a-0d81-4716-a061-a5926e4bbbbf?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "description_attr_id": "0ad59a7f-091c-4311-8e60-9934711371b0",
+                "website": "www.wizcommerce.com",
+                "header": "",
+                "section_header": "Product Details",
+                "img_logo": "",
+                "tenant_address": "3412 Monroe Road\nSuite A\nCharlotte, NC 28205",
+                "sample_file": None,
+                "attrs_to_show":
+                [
+                    "0ad59a7f-091c-4311-8e60-9934711371b0",
+                    "6c1f7233-bf33-47be-a340-98e6b5975ecb",
+                    "bf3959c2-2856-4330-81f1-eab9bcf196b9",
+                    "2b772e48-91e8-482b-9bb9-b6925e2a6cdb",
+                    "8eb96daa-747d-46d0-be93-e22ea4339fdc",
+                    "83eb9a3d-427a-4b34-8266-b53c80bc6397",
+                    "b5ec77ff-2440-483f-a10c-c0f800ffe118",
+                    "177f2544-a0f0-4fa8-a1ae-6117f11e593f"
+                ]
+            },
+            {
+                "product_name": "Optic Diamond Handwoven Cotton Area Rug",
+                "sku_id": "200ASHC07A-P-A212",
+                "product_details": "",
+                "product_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Blue"
+                    },
+                    {
+                        "name": "Material",
+                        "value": "100% Cotton"
+                    }
+                ],
+                "hinge_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Blue"
+                    },
+                    {
+                        "name": "Size",
+                        "value": "5x8 Inch"
+                    }
+                ],
+                "hinges_values": "Blue, 5x8 Inch",
+                "product_hinges":
+                [
+                    {
+                        "name": "Color",
+                        "variants": "Blue, Grey"
+                    },
+                    {
+                        "name": "Size",
+                        "variants": "5x8 Inch, 4x6 Inch, 6x9 Inch"
+                    }
+                ],
+                "is_custom_product": False,
+                "applied_modifiers":
+                [],
+                "group_name": "",
+                "group_number": 0,
+                "priority": 1,
+                "product_image_1": "https://fileserver-g-p.sourcerer.tech/files/3c8103ac-0f14-42b9-977e-e55e105e67be?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "product_image_2": "https://fileserver-g-p.sourcerer.tech/files/be26a070-de6b-42dc-847f-77569c28038f?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "description_attr_id": "0ad59a7f-091c-4311-8e60-9934711371b0",
+                "website": "www.wizcommerce.com",
+                "header": "",
+                "section_header": "Product Details",
+                "img_logo": "",
+                "tenant_address": "3412 Monroe Road\nSuite A\nCharlotte, NC 28205",
+                "sample_file": None,
+                "attrs_to_show":
+                [
+                    "0ad59a7f-091c-4311-8e60-9934711371b0",
+                    "6c1f7233-bf33-47be-a340-98e6b5975ecb",
+                    "bf3959c2-2856-4330-81f1-eab9bcf196b9",
+                    "2b772e48-91e8-482b-9bb9-b6925e2a6cdb",
+                    "8eb96daa-747d-46d0-be93-e22ea4339fdc",
+                    "83eb9a3d-427a-4b34-8266-b53c80bc6397",
+                    "b5ec77ff-2440-483f-a10c-c0f800ffe118",
+                    "177f2544-a0f0-4fa8-a1ae-6117f11e593f"
+                ]
+            },
+            {
+                "product_name": "Broken Lattice Area Rug",
+                "sku_id": "200BDSM04A-P-A63",
+                "product_details": "",
+                "product_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Blue Grey"
+                    },
+                    {
+                        "name": "Material",
+                        "value": "100% Polypropylene"
+                    }
+                ],
+                "hinge_attributes":
+                [
+                    {
+                        "name": "Color",
+                        "value": "Blue Grey"
+                    },
+                    {
+                        "name": "Size",
+                        "value": "9x12 Inch"
+                    }
+                ],
+                "hinges_values": "Blue Grey, 9x12 Inch",
+                "product_hinges":
+                [
+                    {
+                        "name": "Color",
+                        "variants": "Blue Grey, Light Blue, Grey"
+                    },
+                    {
+                        "name": "Size",
+                        "variants": "9x12 Inch, 5x8 Inch, 4x6 Inch"
+                    }
+                ],
+                "is_custom_product": False,
+                "applied_modifiers":
+                [],
+                "group_name": "",
+                "group_number": 0,
+                "priority": 1,
+                "product_image_1": "https://fileserver-g-p.sourcerer.tech/files/3695704d-be87-4137-88a7-6f52baece004?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "product_image_2": "https://fileserver-g-p.sourcerer.tech/files/837d089f-ee2e-451c-bb90-1be45fd7c1ac?quality=50&transforms=%5B%5B%22resize%22%2C%7B%22width%22%3A300%2C%22height%22%3A300%2C%22background%22%3A%22white%22%2C%22fit%22%3A%22contain%22%7D%5D%5D",
+                "description_attr_id": "0ad59a7f-091c-4311-8e60-9934711371b0",
+                "website": "www.wizcommerce.com",
+                "header": "",
+                "section_header": "Product Details",
+                "img_logo": "",
+                "tenant_address": "3412 Monroe Road\nSuite A\nCharlotte, NC 28205",
+                "sample_file": None,
+                "attrs_to_show":
+                [
+                    "0ad59a7f-091c-4311-8e60-9934711371b0",
+                    "6c1f7233-bf33-47be-a340-98e6b5975ecb",
+                    "bf3959c2-2856-4330-81f1-eab9bcf196b9",
+                    "2b772e48-91e8-482b-9bb9-b6925e2a6cdb",
+                    "8eb96daa-747d-46d0-be93-e22ea4339fdc",
+                    "83eb9a3d-427a-4b34-8266-b53c80bc6397",
+                    "b5ec77ff-2440-483f-a10c-c0f800ffe118",
+                    "177f2544-a0f0-4fa8-a1ae-6117f11e593f"
+                ]
+            }
+        ],
+        "updated_at": "2025-01-27T19:07:47.944703"
+    }
